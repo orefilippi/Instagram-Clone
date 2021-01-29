6bab2babacdaa03916d953c05b9902ccb6047557
Orestis Filippi <orefilippi@Orestiss-Mac-mini.local>
2021-01-29 16:55:40 +0000

    finishing behavor of home images

--- frontend/screens/Home.js
+++ frontend/screens/Home.js
@@ -28,17 +28,6 @@ function Home(props) {
         }
     ];
 
-    // const value = useState(new Animated.ValueXY({x:0,y:0})) [0]
-
-    // function moveDot() {
-    //     Animated.timing(value, {
-    //         toValue: {x:50, y:0},
-    //         duration: 1000,
-    //         useNativeDriver: false
-    //     }).start()
-
-    // }
-
 
     return (
 
